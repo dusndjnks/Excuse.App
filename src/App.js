6493b396f06ce8c9bfc3eeb5f543b8e0b962d200
dusndjnks <mhdaffan0000@gmail.com>
2024-07-08 12:10:54 +0530

    deploying app

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ function App() {
   return (
     <div className="App h-screen  flex flex-col justify-center items-center bg-black text-white gap-6">
      <div>
-     <h1 className='text-4xl  text-blue-400 font-thin flex justify-center items-center'>Generate an excuse</h1>
+     <h1 className='text-4xl  text-blue-400 font-thin flex justify-center items-center '>Generate an excuse</h1>
      </div>
      <div className="flex flex-col  items-center gap-2 ">
       <button className="bg-blue-600 rounded-lg  py-1 font-semibold w-[100px]" onClick={()=>{fetchData("funny")}} >Funny</button>
       <button className="bg-pink-600 rounded-lg  py-1  font-semibold w-[130px]" onClick={()=>{fetchData("office")}}>Office</button>
       <button className="bg-red-600 rounded-lg px-5 py-1 font-semibold w-[150px]" onClick={()=>{fetchData("developers")}}>Developers</button>
      </div>
-     <h1 className="text-blue-400 text-2xl font-semibold ">{excuse}</h1>
+     <h1 className="text-blue-400 text-2xl font-semibold w-[250px] text-center">{excuse}</h1>
     </div>
   );
 }
